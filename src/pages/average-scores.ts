a78ca05c47741ca8861cb524e56d1e2b74a8a3d4
Alex Ford <gustavderdrache@gmail.com>
2016-07-13 17:50:28 -0400

    Remove console logging statements

--- src/pages/average-scores.ts
+++ src/pages/average-scores.ts
@@ -3,10 +3,6 @@ import Page from 'views/page';
 import PercentileSection from 'pages/average-scores/percentile-section';
 
 export default class AverageScores extends Page {
-  onShow(): void {
-    console.trace('onShow()');
-  }
-
   onBeforeShow(): void {
     this.pushSection();
 
--- src/pages/average-scores/percentile-section.ts
+++ src/pages/average-scores/percentile-section.ts
@@ -3,8 +3,6 @@ import Figure from 'pages/average-scores/percentile-figure';
 
 export default class PercentileSectionView extends SectionView {
   onBeforeShow(): void {
-    console.log(this);
-
     this.showChildView('inner', new Figure);
   }
 }
