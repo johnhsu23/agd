19bd56953859840e6f3c89aa64c36560e780a5ff
Alex Ford <gustavderdrache@gmail.com>
2016-07-13 16:26:40 -0400

    Prep index.html for sections

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,20 @@
 import * as $ from 'jquery';
+import {Application} from 'backbone.marionette';
 import Figure from 'pages/average-scores/percentile-figure';
 
 $(() => {
-  const figure = new Figure({
-    el: '#figure',
+  const app = new Application;
+
+  app.on('start', () => {
+    const figure = new Figure({
+      el: '#figure',
+    });
+
+    figure.setTitle('Something something chart title...');
+
+    figure.render();
+    figure.triggerMethod('show');
   });
 
-  figure.setTitle('Something something chart title...');
-
-  figure.render();
-  figure.triggerMethod('show');
+  app.start();
 });
