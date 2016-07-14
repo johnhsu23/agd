58cea7c23ec21408eb6c944152f94ea123c17d60
Alex Ford <gustavderdrache@gmail.com>
2016-07-14 15:44:07 -0400

    remove unneeded view

--- src/pages/average-scores/percentile-section.ts
+++
@@ -1,8 +0,0 @@
-import SectionView from 'views/section';
-import Figure from 'pages/average-scores/percentile-figure';
-
-export default class PercentileSectionView extends SectionView {
-  onBeforeShow(): void {
-    this.showChildView('inner', new Figure);
-  }
-}
