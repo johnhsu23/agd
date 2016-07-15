753d9e7e33954daa4c55d3eb72d49ceb9cb0225b
Alex Ford <gustavderdrache@gmail.com>
2016-07-15 12:26:04 -0400

    Eliminate some divitis

--- src/views/figure.ts
+++ src/views/figure.ts
@@ -1,6 +1,10 @@
 import * as template from 'text!templates/figure.html';
 import {LayoutView} from 'backbone.marionette';
+import configure from 'util/configure';
 
+@configure({
+  className: 'figure',
+})
 export default class FigureView extends LayoutView<any> {
   template = () => template;
 
--- src/views/section.ts
+++ src/views/section.ts
@@ -1,6 +1,10 @@
 import {LayoutView} from 'backbone.marionette';
 import * as template from 'text!templates/section.html';
+import configure from 'util/configure';
 
+@configure({
+  className: 'section',
+})
 export default class SectionView extends LayoutView<any> {
   template = () => template;
 
