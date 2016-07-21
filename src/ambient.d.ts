cf6e4962a4f5103da5ba6c52cc55b1794103efd0
Alex Ford <gustavderdrache@gmail.com>
2016-07-21 18:04:45 -0400

    add percentiles commentary

--- src/ambient.d.ts
+++ src/ambient.d.ts
@@ -35,3 +35,9 @@ declare module 'json!commentary/average-scores.json' {
   const content: Dict<Dict<string>>;
   export = content;
 }
+
+declare module 'json!commentary/percentiles.json' {
+  type Dict<T> = { [key: string]: T };
+  const content: Dict<Dict<string>>;
+  export = content;
+}
