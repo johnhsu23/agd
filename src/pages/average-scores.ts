6cec0682d517b4a2dc7b54abb69a019de04c570b
Alex Ford <gustavderdrache@gmail.com>
2016-07-21 18:05:07 -0400

    give sections control over own commentary

--- src/pages/average-scores.ts
+++ src/pages/average-scores.ts
@@ -6,37 +6,23 @@ import TrendFigure from 'pages/average-scores/trend-figure';
 import PercentileFigure from 'pages/average-scores/percentile-figure';
 import SubscaleFigure from 'pages/average-scores/subscale-figure';
 
-import context from 'models/grade';
-
 import * as trendCommentary from 'json!commentary/average-scores.json';
+import * as percentilesCommentary from 'json!commentary/percentiles.json';
 
 export default class AverageScores extends Page {
-  protected trendSection: DefaultSection;
-
   onBeforeShow(): void {
-    if (!this.trendSection) {
-      this.trendSection = new DefaultSection({
-        inner: new TrendFigure,
-      });
-    }
-    this.pushSection(this.trendSection);
+    this.pushSection(new DefaultSection({
+      inner: new TrendFigure,
+      commentary: trendCommentary,
+    }));
 
     this.pushSection(new DefaultSection({
       inner: new PercentileFigure,
+      commentary: percentilesCommentary,
     }));
 
     this.pushSection(new DefaultSection({
       inner: new SubscaleFigure,
     }));
-
-    this.setCommentary(context);
-  }
-
-  initialize(): void {
-    this.listenTo(context, 'change:grade', this.setCommentary);
-  }
-
-  protected setCommentary({grade}: typeof context): void {
-    this.trendSection.setCommentary(trendCommentary['' + grade]);
   }
 }
--- src/views/default-section.ts
+++ src/views/default-section.ts
@@ -3,23 +3,47 @@ import {ViewOptions} from 'backbone';
 
 import Switcher from 'views/grade-switcher';
 import Section from 'views/section';
+import context from 'models/grade';
+
+import configure from 'util/configure';
+
+type Dict<T> = { [key: string]: T };
+type Commentary = Dict<Dict<string>>;
 
 export interface DefaultSectionOptions extends ViewOptions<any> {
   inner: View<any>;
+  commentary?: Commentary;
 }
 
+@configure({
+  model: context,
+  modelEvents: {
+    'change:grade': 'changedGrade',
+  },
+})
 export class DefaultSection extends Section {
-  inner: View<any>;
+  protected inner: View<any>;
+  protected commentary: Commentary;
 
   constructor(options: DefaultSectionOptions) {
     super(options);
 
     this.inner = options.inner;
+    this.commentary = options.commentary;
   }
 
   onBeforeShow(): void {
     this.showChildView('contents', this.inner);
     this.showChildView('controls', new Switcher);
+    if (this.commentary) {
+      this.setCommentary(this.commentary[this.model.grade]);
+    }
+  }
+
+  protected changedGrade(): void {
+    if (this.commentary) {
+      this.setCommentary(this.commentary[this.model.grade]);
+    }
   }
 }
 
