bd73066df300a012fa94c7d14506297a52e7099e
Alex Ford <gustavderdrache@gmail.com>
2016-07-21 17:46:57 -0400

    update commentary

--- src/pages/average-scores.ts
+++ src/pages/average-scores.ts
@@ -6,11 +6,20 @@ import TrendFigure from 'pages/average-scores/trend-figure';
 import PercentileFigure from 'pages/average-scores/percentile-figure';
 import SubscaleFigure from 'pages/average-scores/subscale-figure';
 
+import context from 'models/grade';
+
+import * as trendCommentary from 'json!commentary/average-scores.json';
+
 export default class AverageScores extends Page {
+  protected trendSection: DefaultSection;
+
   onBeforeShow(): void {
-    this.pushSection(new DefaultSection({
-      inner: new TrendFigure,
-    }));
+    if (!this.trendSection) {
+      this.trendSection = new DefaultSection({
+        inner: new TrendFigure,
+      });
+    }
+    this.pushSection(this.trendSection);
 
     this.pushSection(new DefaultSection({
       inner: new PercentileFigure,
@@ -19,5 +28,15 @@ export default class AverageScores extends Page {
     this.pushSection(new DefaultSection({
       inner: new SubscaleFigure,
     }));
+
+    this.setCommentary(context);
+  }
+
+  initialize(): void {
+    this.listenTo(context, 'change:grade', this.setCommentary);
+  }
+
+  protected setCommentary({grade}: typeof context): void {
+    this.trendSection.setCommentary(trendCommentary['' + grade]);
   }
 }
