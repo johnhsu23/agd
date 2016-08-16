95fc5b7207fcc0f1b73ac3bd0d0f8ddbab50c6f7
Alex Ford <gustavderdrache@gmail.com>
2016-08-16 17:30:18 -0400

    complete the d3-ectomy

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,5 +1,4 @@
 /// <reference path="globals/backbone-global/index.d.ts" />
-/// <reference path="globals/d3/index.d.ts" />
 /// <reference path="globals/jquery/index.d.ts" />
 /// <reference path="globals/marionette/index.d.ts" />
 /// <reference path="globals/requirejs/index.d.ts" />
