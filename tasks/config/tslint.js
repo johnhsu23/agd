0fe0f05d2da0abf14e0334d585a9cef5a5823f55
Alex Ford <gustavderdrache@gmail.com>
2016-11-03 12:55:26 -0400

    Add extra pre-build TSLint check
    We don't want console.log() or debugger; statements left over in production code, but we also don't want to ban it during development. Hence, the separate tslint task target.

--- tasks/config/tslint.js
+++ tasks/config/tslint.js
@@ -7,6 +7,38 @@ module.exports = function (grunt) {
         },
         src: ['src/**/*.ts', '!src/baseDir.ts']
       },
+      production: {
+        options: {
+          configuration: {
+            rules: {
+              'no-console': [
+                true,
+                'assert',
+                'clear',
+                'count',
+                'dir',
+                'dirxml',
+                // allow console.error() for error reporting
+                'group',
+                'groupCollapsed',
+                'groupEnd',
+                'info',
+                'log',
+                'profile',
+                'profileEnd',
+                'table',
+                'time',
+                'timeEnd',
+                'timeStamp',
+                'trace',
+                'warn',
+              ],
+              'no-debugger': true,
+            },
+          },
+        },
+        src: ['src/**/*.ts', '!src/baseDir.ts'],
+      },
     },
   });
 }
