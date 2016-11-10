3a7404a708788c331d89f8473a572a51e3f89b54
Michael Drayer <mdrayer@forumone.com>
2016-11-10 16:45:05 -0500

    ETSNAEP-2846 Add Page Level elements to Overall Results page.

--- src/pages/overall-results.ts
+++ src/pages/overall-results.ts
@@ -4,7 +4,10 @@ import DefaultSection from 'views/default-section';
 
 import PercentileFigure from 'pages/overall-results/percentile-figure';
 
+import * as averageCommentary from 'json!commentary/overall-results/average.json';
 import * as percentilesCommentary from 'json!commentary/overall-results/percentiles.json';
+import * as creatingTasksCommentary from 'json!commentary/overall-results/creating-tasks.json';
+import * as respondingTaskCommentary from 'json!commentary/overall-results/responding-task.json';
 
 export default class AverageScores extends Page {
   onBeforeShow(): void {
@@ -14,7 +17,34 @@ export default class AverageScores extends Page {
           download: true,
         },
       }),
-      commentary: percentilesCommentary['visual-arts'],
+      commentary: averageCommentary[this.subject],
+    }));
+
+    this.pushSection(new DefaultSection({
+      inner: new PercentileFigure({
+        share: {
+          download: true,
+        },
+      }),
+      commentary: percentilesCommentary[this.subject],
+    }));
+
+    this.pushSection(new DefaultSection({
+      inner: new PercentileFigure({
+        share: {
+          download: true,
+        },
+      }),
+      commentary: creatingTasksCommentary[this.subject],
+    }));
+
+    this.pushSection(new DefaultSection({
+      inner: new PercentileFigure({
+        share: {
+          download: true,
+        },
+      }),
+      commentary: respondingTaskCommentary[this.subject],
     }));
   }
 }
