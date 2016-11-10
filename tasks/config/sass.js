640443ed56ef35bab03ceefbcf1b0bd9782ed7ab
Russell Stoll <rrstoll@forumone.com>
2016-11-10 12:30:11 -0500

    Initial work

--- tasks/config/sass.js
+++ tasks/config/sass.js
@@ -8,7 +8,7 @@ module.exports = function (grunt) {
       },
       default: {
         files: {
-          'public/style.css': 'sass/main.scss',
+          'public/style.css': 'sass/styles.scss',
         },
       },
     },
