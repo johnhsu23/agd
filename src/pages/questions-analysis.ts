e1cc50be79dd4ba36a0b1b9308b54c88dcd5f473
Michael Drayer <mdrayer@forumone.com>
2016-11-11 16:50:14 -0500

    ETSNAEP-2861 Create questions page elements.

--- src/pages/questions-analysis.ts
+++ src/pages/questions-analysis.ts
@@ -1,5 +1,29 @@
+import {Model} from 'backbone';
+import {ItemView} from 'backbone.marionette';
+
 import Page from 'views/page';
 
-export default class QuestionsAnalysis extends Page {
+import DefaultSection from 'views/default-section';
 
+import * as sampleQuestionsCommentary from 'json!commentary/questions-analysis/sample-questions.json';
+import * as knowledgeSkillsCommentary from 'json!commentary/questions-analysis/knowledge-skills.json';
+import * as performanceCommentary from 'json!commentary/questions-analysis/performance.json';
+
+export default class QuestionsAnalysis extends Page {
+  onBeforeShow(): void {
+    this.pushSection(new DefaultSection({
+      inner: new (class extends ItemView<Model> { template = () => '' }),
+      commentary: sampleQuestionsCommentary[this.subject],
+    }));
+
+    this.pushSection(new DefaultSection({
+      inner: new (class extends ItemView<Model> { template = () => '' }),
+      commentary: knowledgeSkillsCommentary[this.subject],
+    }));
+
+    this.pushSection(new DefaultSection({
+      inner: new (class extends ItemView<Model> { template = () => '' }),
+      commentary: performanceCommentary[this.subject],
+    }));
+  }
 }
