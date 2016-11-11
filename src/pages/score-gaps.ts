f41759db20f353caffde512f12980f1b05fd35bb
Michael Drayer <mdrayer@forumone.com>
2016-11-11 16:12:46 -0500

    ETSNAEP-2859 Create Score Gaps page elements.

--- src/pages/score-gaps.ts
+++ src/pages/score-gaps.ts
@@ -1,5 +1,23 @@
+import {Model} from 'backbone';
+import {ItemView} from 'backbone.marionette';
+
 import Page from 'views/page';
 
-export default class ScoreGaps extends Page {
+import DefaultSection from 'views/default-section';
 
+import * as respondingCommentary from 'json!commentary/score-gaps/responding.json';
+import * as creatingTaskCommentary from 'json!commentary/score-gaps/creating-task.json';
+
+export default class ScoreGaps extends Page {
+  onBeforeShow(): void {
+    this.pushSection(new DefaultSection({
+      inner: new (class extends ItemView<Model> { template = () => '' }),
+      commentary: respondingCommentary[this.subject],
+    }));
+
+    this.pushSection(new DefaultSection({
+      inner: new (class extends ItemView<Model> { template = () => '' }),
+      commentary: creatingTaskCommentary[this.subject],
+    }));
+  }
 }
