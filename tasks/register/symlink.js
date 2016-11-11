ff01d762e43659b836eb7145c125ebc628908f01
Alex Ford <gustavderdrache@gmail.com>
2016-11-11 12:20:22 -0500

    ETSNAEP-2878: Add symlink for CSS source maps

---
+++ tasks/register/symlink.js
@@ -0,0 +1,16 @@
+module.exports = function (grunt) {
+  var fs = require('fs'),
+      path = require('path');
+
+  grunt.registerTask(
+    'symlink',
+    "Creates a symlink to sass/ to enable source map debugging",
+    function () {
+      try {
+        fs.symlinkSync(path.resolve('sass'), 'public/sass', 'dir');
+      } catch (e) {
+        grunt.error(e);
+      }
+    }
+  );
+};
