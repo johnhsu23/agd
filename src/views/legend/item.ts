ebf6fa3b8e3b2e981bf123aab738fba2a30428bd
Alex Ford <gustavderdrache@gmail.com>
2016-11-17 11:31:27 -0500

    ETSNAEP-2889: Simplify legend item class logic

--- src/views/legend/item.ts
+++ src/views/legend/item.ts
@@ -1,18 +1,15 @@
 import D3View from 'views/d3';
 import Legend from 'legends/model';
+import configure from 'util/configure';
 
+@configure({
+  className: 'legend__item',
+})
 abstract class LegendItemView extends D3View<HTMLDivElement, Legend> {
   render(): this {
     super.render();
 
-    const model = this.model,
-          isNote = model.type === 'note';
-
-    this.d3el
-      .attr('data-tag', model.tag || null)
-      .attr('class', null)
-      .classed('legend__note', isNote)
-      .classed('legend__item', !isNote);
+    this.d3el.attr('data-tag', this.model.tag || null);
 
     return this;
   }
