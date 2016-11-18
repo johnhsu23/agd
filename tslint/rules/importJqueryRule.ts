e0e2765f36b0b7434ff6af19990bc04f5d57d111
Alex Ford <gustavderdrache@gmail.com>
2016-11-18 18:13:24 -0500

    Upgrade f1-tslint-misc (again)

--- tslint/rules/importJqueryRule.ts
+++ tslint/rules/importJqueryRule.ts
@@ -9,54 +9,218 @@ export class Rule extends Lint.Rules.AbstractRule {
   }
 }
 
-class ImportJqueryWalker extends Lint.RuleWalker {
-  /**
-   * Indicates the source file imported jQuery as $.
-   */
-  protected sawDollarSign = false;
+interface SawJqueryScope {
+  dollarSign: boolean;
+  jQuery: boolean;
+}
 
-  /**
-   * Indicates the source file imported jQuery.
-   */
-  protected sawJquery = false;
-
-  protected visitImportDeclaration(node: ts.ImportDeclaration): void {
-    // Ensure this is an import of the module named 'jquery'.
-    const specifier = node.moduleSpecifier;
-    if (specifier.kind !== ts.SyntaxKind.StringLiteral || (specifier as ts.StringLiteral).text !== 'jquery') {
-      return;
+class ImportJqueryWalker extends Lint.BlockScopeAwareRuleWalker<SawJqueryScope, SawJqueryScope> {
+  public createBlockScope(): SawJqueryScope {
+    if ((this as {} as { blockScopeStack: {} }).blockScopeStack) {
+      return Object.create(this.getCurrentBlockScope());
+    } else {
+      return Object.create(null);
     }
+  }
 
-    // Ensure there is an import clause, and that it has named bindings (i.e., isn't a default import)
-    const clause = node.importClause;
-    if (!(clause && clause.namedBindings)) {
-      return;
+  public createScope(): SawJqueryScope {
+    if ((this as {} as { scopeStack: {} }).scopeStack) {
+      return Object.create(this.getCurrentScope());
+    } else {
+      return Object.create(null);
     }
+  }
 
-    // Ensure this is a namespace (import * as foo) import.
-    const bindings = clause.namedBindings;
-    if (bindings.kind !== ts.SyntaxKind.NamespaceImport) {
-      return;
-    }
+  protected sawDollarSign(): boolean {
+    return this.getCurrentBlockScope().dollarSign
+        || this.getAllScopes().some(scope => scope.dollarSign);
+  }
 
-    switch (bindings.name.getText()) {
+  protected sawJquery(): boolean {
+    return this.getCurrentBlockScope().jQuery
+        || this.getAllScopes().some(scope => scope.jQuery);
+  }
+
+  protected inspect(scope: SawJqueryScope, text: string): void {
+    switch (text) {
       case '$':
-        this.sawDollarSign = true;
+        scope.dollarSign = true;
         break;
 
       case 'jQuery':
-        this.sawJquery = true;
+        scope.jQuery = true;
         break;
+    }
+  }
+
+  protected visitImportDeclaration(node: ts.ImportDeclaration): void {
+    const scope = this.getCurrentScope();
+
+    const clause = node.importClause;
+
+    // This is an import like "import 'foo';"
+    if (!clause) {
+      return;
+    }
+
+    // Default import
+    if (clause.name) {
+      this.inspect(scope, clause.name.getText());
+    }
+
+    const bindings = clause.namedBindings;
+    if (!bindings) {
+      return;
+    }
+
+    // import * as foo from 'module';
+    if (bindings.kind === ts.SyntaxKind.NamespaceImport) {
+      return this.inspect(scope, bindings.name.getText());
+    }
+
+    // import {...} from 'module';
+    if (bindings.kind === ts.SyntaxKind.NamedImports) {
+      for (const element of bindings.elements) {
+        this.inspect(scope, element.name.getText());
+      }
+    }
+  }
+
+  protected visitClassExpression(node: ts.ClassExpression): void {
+    const scope = this.getCurrentBlockScope();
+
+    if (node.name) {
+      this.inspect(scope, node.name.getText());
+    }
+
+    return super.visitClassExpression(node);
+  }
+
+  protected visitClassDeclaration(node: ts.ClassDeclaration): void {
+    const scope = Object.getPrototypeOf(this.getCurrentBlockScope());
+
+    if (node.name) {
+      this.inspect(scope, node.name.getText());
+    }
+
+    return super.visitClassDeclaration(node);
+  }
+
+  protected visitFunctionExpression(node: ts.FunctionExpression): void {
+    if (node.name) {
+      this.inspect(this.getCurrentScope(), node.name.getText());
+    }
+
+    return super.visitFunctionExpression(node);
+  }
+
+  protected visitFunctionDeclaration(node: ts.FunctionDeclaration): void {
+    const scope = Object.getPrototypeOf(this.getCurrentScope());
+
+    if (node.name) {
+      this.inspect(scope, node.name.getText());
+    }
+
+    return super.visitFunctionDeclaration(node);
+  }
+
+  protected visitParameterDeclaration(node: ts.ParameterDeclaration): void {
+    const scope = this.getCurrentScope();
+
+    const name = node.name;
+    if (name.kind === ts.SyntaxKind.Identifier) {
+      // (name)
+      this.inspect(scope, name.getText());
+    } else {
+      // ({name}) or ([name]) or ...
+      this.inspectBindings(scope, name);
     }
   }
 
   protected visitIdentifier(node: ts.Identifier): void {
     const ident = node.getText(),
-          isFailure = (ident === '$' && !this.sawDollarSign)
-                   || (ident === 'jQuery' && !this.sawJquery);
+          isFailure = (ident === '$' && !this.sawDollarSign())
+                   || (ident === 'jQuery' && !this.sawJquery());
 
     if (isFailure) {
       this.addFailure(this.createFailure(node.getStart(), node.getWidth(), Rule.FAILURE_STRING));
+    }
+  }
+
+  protected visitVariableStatement(node: ts.VariableStatement): void {
+    const list = node.declarationList;
+
+    const isBlockScoped = (list.flags & (ts.NodeFlags.Const | ts.NodeFlags.Let)) !== 0;
+
+    const scope = isBlockScoped ? this.getCurrentBlockScope() : this.getCurrentScope();
+
+    for (const decl of list.declarations) {
+      this.inspectDeclaration(scope, decl);
+    }
+  }
+
+  protected visitMethodDeclaration(node: ts.MethodDeclaration): void {
+    for (const param of node.parameters) {
+      this.visitNode(param);
+    }
+
+    if (node.body) {
+      this.visitNode(node.body);
+    }
+  }
+
+  protected visitPropertyAssignment(node: ts.PropertyAssignment): void {
+    this.visitNode(node.initializer);
+  }
+
+  protected visitPropertyAccessExpression(node: ts.PropertyAccessExpression): void {
+    this.visitNode(node.expression);
+  }
+
+  protected visitPropertyDeclaration(node: ts.PropertyDeclaration): void {
+    if (node.initializer) {
+      super.visitNode(node.initializer);
+    }
+  }
+
+  protected inspectBindings(scope: SawJqueryScope, node: ts.BindingPattern): void {
+    for (const binding of node.elements) {
+      if (binding.kind !== ts.SyntaxKind.OmittedExpression) {
+        this.inspectBinding(scope, binding);
+
+        if (binding.initializer) {
+          this.visitNode(binding.initializer);
+        }
+      }
+    }
+  }
+
+  protected inspectBinding(scope: SawJqueryScope, node: ts.BindingElement | ts.ArrayBindingElement): void {
+    if (node.kind === ts.SyntaxKind.BindingElement) {
+      const name = node.name;
+
+      if (name.kind === ts.SyntaxKind.Identifier) {
+        this.inspect(scope, name.getText());
+      } else {
+        this.inspectBindings(scope, name);
+      }
+
+      if (node.initializer) {
+        this.visitNode(node.initializer);
+      }
+    }
+  }
+
+  protected inspectDeclaration(scope: SawJqueryScope, node: ts.VariableDeclaration) {
+    const name = node.name;
+    if (name.kind === ts.SyntaxKind.Identifier) {
+      this.inspect(scope, name.getText());
+    } else {
+      this.inspectBindings(scope, name);
+    }
+
+    if (node.initializer) {
+      this.visitNode(node.initializer);
     }
   }
 }
