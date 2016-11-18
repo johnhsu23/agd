6bfc1432ab98a3d079d3a7ff720b6603c2a341a9
Alex Ford <gustavderdrache@gmail.com>
2016-11-18 12:16:39 -0500

    ETSNAEP-2889:  Hoist data loading to gaps-figure

--- src/pages/score-gaps/gaps-figure.ts
+++ src/pages/score-gaps/gaps-figure.ts
@@ -4,16 +4,24 @@ import Figure from 'views/figure';
 import Chart from 'pages/score-gaps/gaps-chart';
 import GapSelector from 'pages/score-gaps/gap-selector';
 
+import * as api from 'pages/score-gaps/gaps-data';
+
 import * as vars from 'data/variables';
 
 export default class ScoreGaps extends Figure {
+  protected chart: Chart;
+
   onRender(): void {
     if (super.onRender) {
       super.onRender();
     }
 
-    this.showContents(new Chart);
+    this.chart = new Chart;
+
+    this.showContents(this.chart);
     this.showControls(new GapSelector);
+
+    this.updateChart(vars.SDRACE, 0, 1);
   }
 
   childEvents(): EventsHash {
@@ -23,7 +31,12 @@ export default class ScoreGaps extends Figure {
   }
 
   onChildGapSelect(view: GapSelector, variable: vars.Variable, focal: number, target: number): void {
-    this.getChildView('contents')
-      .trigger('gap:select', variable, focal, target);
+    this.updateChart(variable, focal, target);
+  }
+
+  protected updateChart(variable: vars.Variable, focal: number, target: number): void {
+    api.load(vars.SDRACE, 0, 1)
+      .then(result => this.chart.renderData(result))
+      .done();
   }
 }
