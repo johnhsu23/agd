bb830d8fbacfd079e89c6048c28e866f51b51ea9
Alex Ford <gustavderdrache@gmail.com>
2016-12-02 15:05:02 -0500

    ETSNAEP-2900: Account for more edge cases in wrap

--- src/util/wrap.ts
+++ src/util/wrap.ts
@@ -1,16 +1,16 @@
 import {Selection} from 'd3-selection';
 
 /**
- * Wraps the text inside a given SVG <text> element to fit a particular width.
+ * Wraps the text inside a given SVG `<text>` element to fit a particular width.
  *
- * Note that the <text> elements MUST be attached to the document and visible! If not, they will have no
+ * Note that the `<text>` elements MUST be attached to the document and visible! If not, they will have no
  * text metrics for us to measure, and this function will not wrap any text.
  *
  * @param text A selection of SVG <text> elements
  * @param width The width to constrain to
  */
 export default function wrap<T, U>(text: Selection<SVGTextElement, T, null, U>, width: number): void {
-  text.each(function (this: SVGTextElement) {
+  text.each(function () {
     const boxes: Node[] = [];
     while (this.firstChild) {
       const node = this.firstChild;
@@ -19,7 +19,11 @@ export default function wrap<T, U>(text: Selection<SVGTextElement, T, null, U>, 
         let text = node,
             result: RegExpMatchArray;
 
-        while ((result = text.textContent.match(/\s/)) !== null) {
+        // Split on:
+        // * whitespace
+        // * hyphens
+        // * forward slashes
+        while ((result = text.textContent.match(/[\s-\/]/)) !== null) {
           text = text.splitText(result.index + 1);
 
           const previous = text.previousSibling;
@@ -30,6 +34,8 @@ export default function wrap<T, U>(text: Selection<SVGTextElement, T, null, U>, 
         this.removeChild(text);
         boxes.push(text);
       } else {
+        // Treat non-Text children as their own box; this allows us to wrap around pre-existing <tspan>s
+        // (such as in the case of having to format the 'p' in 'p < .05).
         this.removeChild(node);
         boxes.push(node);
       }
@@ -50,15 +56,30 @@ export default function wrap<T, U>(text: Selection<SVGTextElement, T, null, U>, 
       text.appendChild(box);
 
       if (text.getComputedTextLength() > width) {
-        text.removeChild(box);
         line++;
 
+        // If a single word overflows the text element, force it to be output in its own line
+        const shouldMove = text.childNodes.length > 1;
+        if (shouldMove) {
+          text.removeChild(box);
+        }
+
+        // Trim whitespace at the end of a line.
+        // Doing this here instead of earlier does mean that text metrics are slightly off, but taking the action at all
+        // will help ease some of the more pathological cases of long words wrapped to fairly narrow column widths.
+        text.textContent = text.textContent.trim();
+
+        // Allocate a new line
         text = document.createElementNS('http://www.w3.org/2000/svg', 'tspan');
         this.appendChild(text);
         copyList.call(this, x, text.x.baseVal);
         copyList.call(this, y, text.y.baseVal);
 
-        text.appendChild(box);
+        if (shouldMove) {
+          // This is the second half of the move operation from above, so it only
+          // occurs if we removed the box from the previous `text' element.
+          text.appendChild(box);
+        }
         text.setAttribute('dy', (line * lineHeight) + 'em');
       }
     }
