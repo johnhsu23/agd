5479a84da4485beee29c6adf300fc6892546d392
Michael Drayer <mdrayer@forumone.com>
2016-12-07 17:01:47 -0500

    ETSNAEP-2938 Add white background, legend, bar text. Adjust chart sizing.

--- src/pages/student-experiences/trends-chart.ts
+++ src/pages/student-experiences/trends-chart.ts
@@ -1,16 +1,13 @@
 import {ViewOptions, Model} from 'backbone';
 import {Selection} from 'd3-selection';
-import {scaleBand, scaleOrdinal} from 'd3-scale';
-import {axisLeft} from 'd3-axis';
+import {scaleBand, scaleLinear} from 'd3-scale';
+import {axisLeft, axisBottom} from 'd3-axis';
 import {stack, SeriesPoint} from 'd3-shape';
-//import {range} from 'underscore';
 
 import configure from 'util/configure';
 import Chart from 'views/chart';
 import {ContextualVariable} from 'data/contextual-variables';
-
-import * as scales from 'components/scales';
-import * as axis from 'components/axis';
+import wrap from 'util/wrap';
 
 import {load, Grouped, StackedDatum} from 'pages/student-experiences/trends-data';
 
@@ -19,13 +16,13 @@ export interface TrendsChartOptions extends ViewOptions<Model> {
 }
 
 @configure({
-  className: 'chart chart--bar',
+  className: 'chart chart--bar chart--bar--stacked',
 })
 export default class TrendsChart extends Chart<Model> {
-  protected marginLeft = 50;
+  protected marginLeft = 40;
   protected marginRight = 25;
-  protected marginBottom = 50;
-  protected marginTop = 65;
+  protected marginBottom = 30;
+  protected marginTop = 50;
 
   protected percentAxis: Selection<SVGGElement, {}, null, void>;
   protected yearAxis: Selection<SVGGElement, {}, null, void>;
@@ -61,14 +58,14 @@ export default class TrendsChart extends Chart<Model> {
     const years = [2016, 2008];
 
     // setup and add the x axis
-    const percent = scales.percent()
-      .bounds([0, 100])
-      .domain([0, 100]);
+    const percent = scaleLinear()
+      .domain([0, 100])
+      .range([0, 500]);
 
-    const percentAxis = axis.horizontalBottom()
-      .scale(percent);
+    const percentAxis = axisBottom(percent);
 
-    const chartHeight = 300,
+    // set chart height and width
+    const chartHeight = 160,
         chartWidth = percent.range()[1];
 
     this.height(chartHeight)
@@ -82,7 +79,7 @@ export default class TrendsChart extends Chart<Model> {
     const year = scaleBand<number>()
       .domain(years)
       .range([0, chartHeight])
-      .padding(0.5);
+      .padding(0.2);
 
     const yearAxis = axisLeft(year);
 
@@ -90,23 +87,24 @@ export default class TrendsChart extends Chart<Model> {
       .attr('transform', `translate(${this.marginLeft}, ${this.marginTop})`)
       .call(yearAxis);
 
-    // stack stuff
-    // temporary colors just for testing the stacks
-    const colorsArray = ['#98abc5', '#8a89a6', '#7b6888', '#6b486b', '#a05d56', '#d0743c', '#ff8c00'];
+    // apply a white background before we do anything else
+    this.selectAll('.background')
+      .data([0])
+      .enter().insert('rect', '.chart__inner')
+        .attr('width', '100%')
+        .attr('height', '100%')
+        .attr('fill', 'white');
 
-    const colors = scaleOrdinal()
-      .range(colorsArray.slice(0, this.variable.categories.length))
-      .domain(this.variable.categories);
-
+    // now begins the stacking
     const seriesData = stack().keys(this.variable.categories)(data.stackData);
 
     const series = this.inner.selectAll('.series')
-      .data(seriesData);
+      .data(seriesData)
+      .enter().append('g')
+        .attr('class', (_, i) => `series series--${i}`);
 
-    series.enter().append('g')
-        .attr('class', 'series')
-        .attr('fill', d => '' + colors(d.key))
-      .selectAll('rect')
+    // add bars
+    series.selectAll('rect')
       // NB. Workaround for https://github.com/Microsoft/TypeScript/issues/12713
       .data(d => d as SeriesPoint<StackedDatum>[])
       .enter().append('rect')
@@ -114,6 +112,37 @@ export default class TrendsChart extends Chart<Model> {
         .attr('x', d => percent(d[0]))
         .attr('height', year.bandwidth())
         .attr('width', d => percent(d[1]) - percent(d[0]));
-  }
 
+    // add text on top of bars
+    series.selectAll('.bar--text')
+      .data(d => d as SeriesPoint<StackedDatum>[])
+      .enter().append('text')
+        .attr('text-anchor', 'middle')
+        .classed('value', true)
+        .attr('x', d => (percent(d[1]) + percent(d[0])) / 2)
+        .attr('y', d => year(d.data['year']) + 35)
+        .text(d => Math.round(d[1] - d[0]));
+
+    // place legend for the series
+    const legend = series.append('g')
+      .attr('class', 'legend')
+      .attr('transform', d => {
+        const datum = d[1];
+        const x = (percent(datum[1]) + percent(datum[0])) / 2;
+        const y = -20;
+        return `translate(${x}, ${y})`;
+      });
+
+    legend.append('line')
+      .attr('y1', 6)
+      .attr('y2', 26);
+
+    legend.append('text')
+      .attr('text-anchor', 'middle')
+      .text(d => d.key);
+
+    // wrap text to fit within the bar
+    legend.selectAll('text')
+      .call(wrap, 20);
+  }
 }
