76d85024e4d5e3c9f48de8296bbc4159d8673b5c
Alex Ford <gustavderdrache@gmail.com>
2016-12-19 12:42:57 -0500

    ETSNAEP-2893: Add 'Info not available' category

--- src/data/variables.ts
+++ src/data/variables.ts
@@ -76,6 +76,7 @@ export const SLUNCH3: Variable = {
   categories: [
     'Eligible for National School Lunch Program (NSLP)',
     'Not eligible for National School Lunch Program (NSLP)',
+    'Information not available',
   ],
 };
 
@@ -163,7 +164,7 @@ export const SLUNCH1: Variable = {
   categories: [
     'Eligible for National School Lunch Program (NSLP)',
     'Not eligible for National School Lunch Program (NSLP)',
-    'No information',
+    'Information not available',
   ],
 };
 
