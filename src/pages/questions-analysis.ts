b37146b145ab63929d96e47e23ce52eaeeadb3ae
Michael Drayer <mdrayer@forumone.com>
2016-12-19 15:06:54 -0500

    ETSNAEP-2863 Add goToAccordion functionality on item link click. Update links.

--- src/pages/questions-analysis.ts
+++ src/pages/questions-analysis.ts
@@ -1,6 +1,8 @@
 import Page from 'views/page';
 import DefaultSection from 'views/default-section';
 import NotesSourcesView from 'views/notes-sources';
+import {EventsHash} from 'backbone';
+import * as $ from 'jquery';
 
 import context from 'models/context';
 import {questions} from 'data/sample-questions';
@@ -37,5 +39,28 @@ export default class QuestionsAnalysis extends Page {
     this.showChildView('footer', new NotesSourcesView({
       contents: (context.subject === 'music') ? questionsMusicNotes : questionsVisualArtsNotes,
     }));
+  }
+
+  events(): EventsHash {
+    return {
+      'click .item-map__item__link a': 'goToAccordion',
+    };
+  }
+
+  goToAccordion(event: JQueryMouseEventObject): void {
+    // get the accordion based on the ID on the link
+    const accordionId = event.target.getAttribute('data-accordion-id');
+    const accordion = this.$(`[data-index="${accordionId}"]`);
+
+    // check if accordion is NOT already expanded
+    if (accordion.find('.is-expanded').length === 0) {
+      //open the accordion (trigger click on the data-accordion-header anchor)
+      accordion.find('[data-accordion-header]').click();
+    }
+
+    // have the page scroll to the accordion
+    $(window).scrollTop(accordion.offset().top);
+
+    event.preventDefault();
   }
 }
