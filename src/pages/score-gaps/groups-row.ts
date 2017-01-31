6607b8d104e2d816e88988797bbbcbcfaa175e12
Michael Drayer <mdrayer@forumone.com>
2017-01-31 10:51:19 -0500

    ETSNAEP-3115 Rename School Type categories for Student Groups Score Trends table. Add indent class to Catholic th element.

--- src/pages/score-gaps/groups-row.ts
+++ src/pages/score-gaps/groups-row.ts
@@ -4,6 +4,7 @@ import * as $ from 'jquery';
 import configure from 'util/configure';
 import noTemplate from 'util/no-template';
 import {formatValue} from 'codes';
+import {studentGroupsById} from 'data/variables';
 
 import GroupsModel from 'pages/score-gaps/groups-model';
 import {Data} from 'pages/score-gaps/groups-data';
@@ -27,8 +28,23 @@ export default class RowView extends ItemView<GroupsModel> {
           variable = model.variable,
           category = model.category;
 
+    let categoryLabel = variable.categories[category],
+        shouldIndent = false;
+
+    if (variable === studentGroupsById['SCHTYPE']) {
+      // rename the school type categories for this table only
+      if (category === 1) {
+        categoryLabel = 'Private';
+      } else if (category === 2) {
+        categoryLabel = 'Catholic';
+        // indent 'Catholic' label to show as subset of 'Private'
+        shouldIndent = true;
+      }
+    }
+
     $('<th>', { scope: 'row' })
-      .text(variable.categories[category])
+      .text(categoryLabel)
+      .toggleClass('indent', shouldIndent)
       .appendTo(this.$el);
 
     for (const key of keys) {
