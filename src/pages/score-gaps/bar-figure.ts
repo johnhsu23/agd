1eeb3d49a4e221d75a49a9ad9884121f1bda14a7
Michael Drayer <mdrayer@forumone.com>
2017-03-08 14:18:38 -0500

    ETSNAEP-3285 Change 'American Indian/Alaska Native' creating task data to 999. Include legend for bar chart.

--- src/pages/score-gaps/bar-figure.ts
+++ src/pages/score-gaps/bar-figure.ts
@@ -1,13 +1,17 @@
-import {EventsHash} from 'backbone';
+import {Collection, EventsHash} from 'backbone';
 
 import Figure from 'views/figure';
 import Chart from 'pages/score-gaps/bar-chart';
 import VariableSelector from 'views/variable-selector';
-
+import LegendView from 'views/legend';
+import {all as gatherNotes} from 'legends/gather';
 import * as vars from 'data/variables';
+
+import {groupedData} from 'pages/score-gaps/bar-data';
 
 export default class TaskBar extends Figure {
   protected variable: vars.Variable = vars.SDRACE;
+  protected legendCollection = new Collection;
 
   onRender(): void {
     if (super.onRender) {
@@ -28,6 +32,11 @@ export default class TaskBar extends Figure {
       }
     });
     this.showControls(new VariableSelector({ variables: studentGroups }));
+
+    this.showLegend(new LegendView({
+      collection: this.legendCollection,
+    }));
+    this.updateLegend();
   }
 
   childEvents(): EventsHash {
@@ -41,10 +50,16 @@ export default class TaskBar extends Figure {
     this.getChildView('contents')
       .trigger('variable:select', variable);
     this.setTitle(this.makeTitle());
+    this.updateLegend();
   }
 
   protected makeTitle(): string {
     return 'Average creating task scores for eighth-grade students assessed in NAEP ' +
       `visual arts, by ${this.variable.title}: 2016`;
+  }
+
+  protected updateLegend(): void {
+    const legends = gatherNotes(groupedData[this.variable.id], row => row.errorFlag, () => '');
+    this.legendCollection.reset(legends);
   }
 }
