8802b8c917114aefa25ae0131a366d6f2c5d0c01
Katrina Hussain <KatrinaH@users.noreply.github.com>
2017-03-17 15:38:56 -0400

    [ETSNAEP-3327] add chart data

--- src/pages/overall-results/responding-data.ts
+++ src/pages/overall-results/responding-data.ts
@@ -1,6 +1,7 @@
-export const data = [38.43740, 48.30210, 54.70840, 62.10940];
+export const data = [50.93140, 38.43740, 48.30210, 54.70840, 62.10940];
 
 export const scoreText = [
+  'Overall',
   '<= 25th percentile',
   '> 25th and <= 50th percentile',
   '> 50th and <= 75th percentile',
