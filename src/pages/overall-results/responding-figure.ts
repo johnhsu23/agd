c519885f67399d30c7f8b7670d7ea6ce12c9d7ef
Katrina Hussain <KatrinaH@users.noreply.github.com>
2017-03-21 08:56:16 -0400

    [ETSNAEP-3328] update chart title

--- src/pages/overall-results/responding-figure.ts
+++ src/pages/overall-results/responding-figure.ts
@@ -4,7 +4,8 @@ import Chart from 'pages/overall-results/responding-chart';
 
 export default class RespondingFigure extends Figure {
   protected makeTitle(): string {
-    return `Responding and creating task scores in ${context.subject} visual arts are related`;
+    return `Average creating task scores for eighth-grade students assessed in
+    NAEP ${context.subject}, by responding score level: 2016`;
   }
 
   onBeforeShow(): void {
